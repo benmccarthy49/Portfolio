7d2bbc5a75285c739c4931e79c55c115e5d4d8e9
Ben McCarthy <b.mccarthy4@lancaster.ac.uk>
2025-03-31 20:35:36 +0100

    Removed comments

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 console.log("Page Loaded");
 
-
-// const aboutMe = document.getElementById("about-me").addEventListener("click", myFunction);
-// const social = document.getElementById("socials").addEventListener("click", myFunction);
-// const experience = document.getElementById("experience").addEventListener("click", myFunction);
-// const languages = document.getElementById("languages-link").addEventListener("click", myFunction);
-// const projects = document.getElementById("project-link").addEventListener("click", myFunction);
-
 const navBar = document.getElementsByTagName("li");
 for(let link of navBar){
     link.addEventListener("click", myFunction);
